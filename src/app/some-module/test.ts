9c3922bb2bea8a870365cdc2036c9e941d72e6e4
selvan-g2t <36733479+selvan-g2t@users.noreply.github.com>
2018-05-08 19:15:05 +0530

    fsdf

--- src/app/some-module/test.ts
+++ src/app/some-module/test.ts
@@ -1,1 +1,35 @@
-import {Component, NgModule, VERSION} from '@angular/core'; import { Routes, RouterModule } from '@angular/router'; import {BrowserModule} from '@angular/platform-browser'; import { CommonModule } from '@angular/common'; @Component({ selector: 'testing', template: ` <div> <h2>Hello {{name}}</h2> </div> `, }) export class TestingComponents{ name:string; constructor() { this.name = `Angular! v${VERSION.full}` } } const routes: Routes = [ { path: '', redirectTo: 'one', pathMatch: 'full' }, { path: 'one', component: TestingComponents } ]; @NgModule({ imports: [ CommonModule,RouterModule.forChild(routes) ], declarations: [ TestingComponents ], bootstrap: [ TestingComponents ] }) export class TestingModule {}
+//our root app component
+import {Component, NgModule, VERSION} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'testing',
+  template: `
+    <div>
+      <h2>Hello {{name}}</h2>
+    </div>
+  `,
+})
+export class TestingComponents{
+  name:string;
+  constructor() {
+    this.name = `Angular! v${VERSION.full}`
+  }
+}
+
+const routes: Routes = [
+    { path: '', redirectTo: 'one', pathMatch: 'full' },
+    {
+      path: 'one',
+      component: TestingComponents
+    }
+  ];
+  
+
+@NgModule({
+  imports: [ CommonModule,RouterModule.forChild(routes) ],
+  declarations: [ TestingComponents ],
+  bootstrap: [ TestingComponents ]
+})
